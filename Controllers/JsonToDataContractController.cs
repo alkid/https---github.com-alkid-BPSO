91a618c7f3bb0e6defe2b8786cab3a0447bf82d7
alkid <alkidademi@gmail.com>
2014-11-28 14:00:45 +0100

    RNA

---
+++ Controllers/JsonToDataContractController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RNA.Controllers
+{
+    public class JsonToDataContractController : ApiController
+    {
+        // POST api/<controller>
+        public HttpResponseMessage Post(HttpRequestMessage value)
+        {
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            response.Content = new StringContent("I'll be running on an Azure Web Site!");
+            return response;
+        }
+
+        public HttpResponseMessage Get()
+        {
+            var response = this.Request.CreateResponse(HttpStatusCode.OK);
+            response.Content = new StringContent("I'll be running on an Azure Web Site!");
+            return response;
+        }
+    }
+}
